70a0a8163129fc2e7239ef5156e4a803f5187d0c
Victor Castillo <victorcastilloaguero@gmail.com>
2023-01-25 21:09:23 +0100

    fixed border rendering

--- src/layout/components/components.cpp
+++ src/layout/components/components.cpp
@@ -14,7 +14,7 @@ ComponentState::ComponentState() {
 }
 
 void ComponentState::dirty() {
-    _dirty = true;
+  _dirty = true;
 }
 
 //===== COMPONENTS
@@ -32,98 +32,98 @@ void ComponentState::dirty() {
 //}
 
 Component::Component()
-        : Component([](Component*) {
+  : Component([](Component*) {
 }) {
 }
 
 Component::Component(std::function<void(Component*)> inner)
-        : Component(new ComponentState(), inner) {
-    state->stateless_comp = true;
+  : Component(new ComponentState(), inner) {
+  state->stateless_comp = true;
 }
 
 Component::Component(ComponentState* state)
-        : Component(state, [](Component*) {
+  : Component(state, [](Component*) {
 }) {
 }
 
 Component::Component(ComponentState* state, std::function<void(Component*)> inner)
-        : state(state), dim(&state->dim) {
-    state->component_instance = this;
-    this->inner_redraw = std::move(inner);
-    parent = nullptr;
-
+  : state(state), dim(&state->dim) {
+  state->component_instance = this;
+  this->inner_redraw = std::move(inner);
+  parent = nullptr;
+  
 }
 
 //== Destructor
 Component::~Component() {
-    for (auto &child: children)
-        delete child;
-    children.clear();
-    //for (auto &child: param_children)
-    //  delete child;
-    //param_children.clear();
-    state->component_instance = nullptr;
-
-    if (state->stateless_comp)
-        delete state;
+  for (auto &child: children)
+    delete child;
+  children.clear();
+  //for (auto &child: param_children)
+  //  delete child;
+  //param_children.clear();
+  state->component_instance = nullptr;
+  
+  if (state->stateless_comp)
+    delete state;
 }
 
 //== API for subclasses
 Component* Component::new_group() {
-    auto* c = new Component();
-    c->is_group = true;
-    return c;
+  auto* c = new Component();
+  c->is_group = true;
+  return c;
 }
 
 void Component::add(std::vector<Component*> ichildren) {
-    // TODO - Needs to be recursive when flattening groups, not just first layer
-    for (auto &item: ichildren) {
-        if (item == nullptr) continue;
-        if (item->is_group) {
-            for (auto &subitem: item->children) {
-                if (subitem == nullptr || !subitem->state) continue;
-                subitem->parent = this;
-                this->children.push_back(subitem);
-            }
-            item->children.clear();
-            delete item;
-        } else {
-            item->parent = this;
-            this->children.push_back(item);
-        }
+  // TODO - Needs to be recursive when flattening groups, not just first layer
+  for (auto &item: ichildren) {
+    if (item == nullptr) continue;
+    if (item->is_group) {
+      for (auto &subitem: item->children) {
+        if (subitem == nullptr || !subitem->state) continue;
+        subitem->parent = this;
+        this->children.push_back(subitem);
+      }
+      item->children.clear();
+      delete item;
+    } else {
+      item->parent = this;
+      this->children.push_back(item);
     }
+  }
 }
 
 void Component::redraw() {
-    inner_redraw(this);
-    on_redraw();
-
-    for (auto &child: children) {
-        child->redraw();
-    }
-    state->_dirty = false;
+  inner_redraw(this);
+  on_redraw();
+  
+  for (auto &child: children) {
+    child->redraw();
+  }
+  state->_dirty = false;
 }
 
 void Component::render(const cydui::window::CWindow* win) {
-    auto* win_ref = win->win_ref;
-
-    for (auto &child: children) {
-        if (child) child->render(win);
-    }
-
-    if (state != nullptr && state->border.enabled) {
-        graphics::drw_rect(
-                win_ref,
-                state->border.color,
-                state->dim.x.val() + state->dim.margin.left.val(),
-                state->dim.y.val() + state->dim.margin.top.val(),
-                state->dim.w.val() - state->dim.margin.left.val() - state->dim.margin.right.val() + 2,
-                state->dim.h.val() - state->dim.margin.top.val() - state->dim.margin.bottom.val() + 1,
-                false
-        );
-    }
-
-    on_render(win);
+  auto* win_ref = win->win_ref;
+  
+  for (auto &child: children) {
+    if (child) child->render(win);
+  }
+  
+  if (state != nullptr && state->border.enabled) {
+    graphics::drw_rect(
+      win_ref,
+      state->border.color,
+      state->dim.cx.val() - state->dim.padding.left.val(),
+      state->dim.cy.val() - state->dim.padding.top.val(),
+      state->dim.cw.val() + state->dim.padding.left.val() + state->dim.padding.right.val() + 2,
+      state->dim.ch.val() + state->dim.padding.top.val() + state->dim.padding.bottom.val() + 1,
+      false
+    );
+  }
+  
+  on_render(win);
 }
 
 void Component::on_render(const cydui::window::CWindow* win) {
@@ -139,26 +139,26 @@ void Component::on_key_release() {
 }
 
 void Component::on_mouse_enter(int x, int y) {
-    // TODO - Must change coords since they are relative
-    if (this->parent)
-        this->parent->on_mouse_enter(x, y);
+  // TODO - Must change coords since they are relative
+  if (this->parent)
+    this->parent->on_mouse_enter(x, y);
 }
 
 void Component::on_mouse_click(int x, int y, int button) {
-    // TODO - Must change coords since they are relative
-    if (this->parent)
-        this->parent->on_mouse_click(x, y, button);
+  // TODO - Must change coords since they are relative
+  if (this->parent)
+    this->parent->on_mouse_click(x, y, button);
 }
 
 void Component::on_mouse_exit(int x, int y) {
-    // TODO - Must change coords since they are relative
-    if (this->parent)
-        this->parent->on_mouse_exit(x, y);
+  // TODO - Must change coords since they are relative
+  if (this->parent)
+    this->parent->on_mouse_exit(x, y);
 }
 
 void Component::on_scroll(int d) {
-    if (this->parent)
-        this->parent->on_scroll(d);
+  if (this->parent)
+    this->parent->on_scroll(d);
 }
 
 //Component* Component::set_size(int w, int h) {
@@ -248,10 +248,10 @@ void Component::on_scroll(int d) {
 ///=====================================================================
 
 Component* Component::set_border_enable(bool enabled) {
-    state->border.enabled = enabled;
-    return this;
+  state->border.enabled = enabled;
+  return this;
 }
 
 Component* Component::get_parent() {
-    return parent;
+  return parent;
 }
