4f88ce1b7acdc0d74d3dfca6d10c8efd196eb553
Victor Castillo <victorcastilloaguero@gmail.com>
2024-03-01 00:02:49 +0100

    added 'with' specialization for std::variant

--- include/core/with_specialization.h
+++ include/core/with_specialization.h
@@ -165,6 +165,71 @@ namespace cydui::components::with {
         return *this;
       }
     };
+    
+    template<typename ...Ts>
+    struct with<std::variant<Ts...>>: public with_data_t<std::variant<Ts...>> {
+      template<typename T>
+      with<std::variant<Ts...>> &
+      if_type_is(const std::function<std::vector<cydui::components::component_builder_t>(T &value)> &builder) {
+        std::variant<Ts...> v = this->val;
+        if (std::holds_alternative<T>(this->val)) {
+          T &value = std::get<T>(this->val);
+          auto cs = builder(value);
+          std::string id;
+          std::size_t i = 0;
+          std::string index_suffix;
+          id = ":if_type_is<";
+          id.append(std::string {typeid(T).name()});
+          id = ">";
+          
+          for (const auto &item1: cs) {
+            index_suffix = ":";
+            index_suffix.append(std::to_string(i));
+            
+            for (const auto &component_pair: item1.get_components()) {
+              auto [_id, component] = component_pair;
+              // make copy of id for modification
+              std::string final_id = id;
+              final_id.append(":");
+              final_id.append(_id);
+              this->selection.emplace_back(final_id, component);
+            }
+            ++i;
+          }
+        }
+        return *this;
+      }
+      template<typename T>
+      with<std::variant<Ts...>> &
+      if_type_is(std::vector<cydui::components::component_builder_t> &cs) {
+        std::variant<Ts...> v = this->val;
+        if (std::holds_alternative<T>(this->val)) {
+          T &value = std::get<T>(this->val);
+          std::string id;
+          std::size_t i = 0;
+          std::string index_suffix;
+          id = ":if_type_is<";
+          id.append(std::string {typeid(T).name()});
+          id = ">";
+          
+          for (const auto &item1: cs) {
+            index_suffix = ":";
+            index_suffix.append(std::to_string(i));
+            
+            for (const auto &component_pair: item1.get_components()) {
+              auto [_id, component] = component_pair;
+              // make copy of id for modification
+              std::string final_id = id;
+              final_id.append(":");
+              final_id.append(_id);
+              this->selection.emplace_back(final_id, component);
+            }
+            ++i;
+          }
+        }
+        return *this;
+      }
+    };
 }
 
 #endif //CYD_UI_WITH_SPECIALIZATION_H
