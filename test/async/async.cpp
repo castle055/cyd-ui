2e6791631e9ad5aa58150fa3d53c35bd4bb00bac
Victor Castillo <victorcastilloaguero@gmail.com>
2024-04-03 05:37:34 +0200

    added CTest

---
+++ test/async/async.cpp
@@ -0,0 +1,9 @@
+//
+// Created by castle on 4/3/24.
+//
+
+#include "common.h"
+
+int main(int argc, char** argv) {
+  return 0;
+}
---
+++ test/common/common.h
@@ -0,0 +1,11 @@
+//
+// Created by castle on 4/3/24.
+//
+
+#ifndef CYD_UI_COMMON_H
+#define CYD_UI_COMMON_H
+
+namespace test {
+}
+
+#endif //CYD_UI_COMMON_H
