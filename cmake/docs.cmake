015a6e8e9a9afcc38648c016dce70e40307be298
Victor Castillo <victorcastilloaguero@gmail.com>
2024-04-04 19:56:13 +0200

    cmake update

--- cmake/docs.cmake
+++ cmake/docs.cmake
@@ -1,11 +1,10 @@
-add_custom_target(cyd_ui_docs_dir
-        ALL COMMAND ${CMAKE_COMMAND} -E make_directory docs
-        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
-)
 add_custom_target(
         cyd_ui_docs
         COMMAND /usr/bin/doxygen ${CMAKE_CURRENT_LIST_DIR}/../Doxyfile
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
         COMMENT "Generating DOXYGEN documentation..."
 )
-add_dependencies(cyd_ui_docs cyd_ui_docs_dir)
+add_custom_command(TARGET cyd_ui_docs PRE_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory docs
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
+)
