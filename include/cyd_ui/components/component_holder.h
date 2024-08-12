3138ba91f2a0f8e7659488f22230b25a3633f9c0
Victor Castillo <victorcastilloaguero@gmail.com>
2024-08-13 00:03:53 +0200

    component_holder: move constructors

--- include/cyd_ui/components/component_holder.h
+++ include/cyd_ui/components/component_holder.h
@@ -6,11 +6,14 @@
 
 #include "with_template.h"
 #include "memory"
+#include <format>
 
 namespace cyd::ui::components {
     struct component_builder_t {
+
+
       template<ComponentConcept C>
-      inline component_builder_t(C comp) { // NOLINT(*-explicit-constructor)
+      component_builder_t(C comp) { // NOLINT(*-explicit-constructor)
         components.emplace_back("", [=] {
           return new C {comp};
         });
@@ -19,7 +22,7 @@ namespace cyd::ui::components {
       }
       
       template<typename T>
-      inline component_builder_t(with::with<T> _with) { // NOLINT(*-explicit-constructor)
+      component_builder_t(with::with<T> _with) { // NOLINT(*-explicit-constructor)
         for (const auto &item: _with.get_selection()) {
           components.emplace_back(item);
         }
@@ -54,21 +57,29 @@ namespace cyd::ui::components {
       //  components[""] = new C {comp};
       //  //components[""] = std::make_unique<C>(comp);
       //}
-      
       template<ComponentConcept C>
-      inline component_holder_t(C comp) { // NOLINT(*-explicit-constructor)
+      component_holder_t(std::vector<C> comps) noexcept {
+        std::size_t index = 0;
+        for (auto& comp : comps) {
+          components.emplace_back(std::to_string(index), new C {comp});
+          ++index;
+        }
+      }
+
+      template<ComponentConcept C>
+      component_holder_t(C comp) { // NOLINT(explicit-constructor)
         components.emplace_back("", new C {comp});
         //components[""] = std::make_unique<C>(comp);
       }
       
       template<typename T>
-      inline component_holder_t(with::with<T> _with) { // NOLINT(*-explicit-constructor)
+      component_holder_t(with::with<T> _with) { // NOLINT(*-explicit-constructor)
         for (const auto &item: _with.get_selection()) {
           components.emplace_back(item.first, item.second());
         }
       }
       
-      inline component_holder_t(const component_builder_t &builder) { // NOLINT(*-explicit-constructor)
+      component_holder_t(const component_builder_t &builder) { // NOLINT(*-explicit-constructor)
         for (const auto &item: builder.get_components()) {
           components.emplace_back(item);
         }
